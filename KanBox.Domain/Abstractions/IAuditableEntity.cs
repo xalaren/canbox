c347c25284e8d8dc89f10584063cd2bf42e9b54c
xalaren <imanov.03.valera@gmail.com>
2026-07-07 23:57:45 +0200

    Added abstractions and covered with tests

---
+++ KanBox.Domain/Abstractions/IAuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace KanBox.Domain.Abstractions
+{
+    /// <summary>
+    /// Represents auditable entities which tracks CreatedOn and ModifiedOn date and times
+    /// </summary>
+    public interface IAuditableEntity
+    {
+        /// <summary>
+        /// CreatedOn UTC date and time
+        /// </summary>
+        DateTime CreatedOnUtc { get; set; }
+        
+        /// <summary>
+        /// ModifiedOn UTC date and time
+        /// </summary>
+        DateTime ModifiedOnUtc { get; set; }
+    }
+}
---
+++ KanBox.Domain/Abstractions/IValidatable.cs
@@ -0,0 +1,14 @@
+﻿namespace KanBox.Domain.Abstractions;
+
+/// <summary>
+/// Require instance validation interface 
+/// </summary>
+public interface IValidatable
+{
+    /// <summary>
+    /// Validate the instance
+    /// </summary>
+    /// <returns><br/><b>True</b> - if instance is valid</returns>
+    /// <returns><br/><b>False</b> - if instance is not valid</returns>
+    bool IsValid();
+}
